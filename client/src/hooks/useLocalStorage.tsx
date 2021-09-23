c4e34b8562a20215237a7e8ec5e1a0574bc5c9c6
DMzS <domenicoenriquemeza20014@gmail.com>
2021-09-23 09:12:03 -0400

    feat: Create local storage handler w/ custom hook
    Added a custom hook to handle the localStorage.
    
    The functionality is very easy, you create an instance of the
    [UseLocalStorage] with the proper props and you'r done!!!
    
    By default it saves a prefixed version of the given [key]
    ('chatappclone'), this happens to prevent an unwanted override.
    
    The function could receive a function that will be executed and then
    saved as a JSON or a variable that will parse and save.
    
    For more information read the documentation here: https://reactjs.org/docs/hooks-custom.html

---
+++ client/src/hooks/useLocalStorage.tsx
@@ -0,0 +1,27 @@
+import {useEffect, useState} from "react";
+
+const PREFIX = "chatappclone";
+
+interface useLocalStorageProps {
+  key: String;
+  initialValue?: Function | any;
+}
+
+function useLocalStorage({ key, initialValue }: useLocalStorageProps) {
+  const prefixedKey = PREFIX + key;
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+    if (typeof initialValue === "function") return initialValue();
+
+    return initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
